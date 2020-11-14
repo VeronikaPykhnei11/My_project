fa89e8de325b2cc74996133af168dd8d602deb52
Veronika <veronikaforkutsaa@gmail.com>
2020-11-14 04:53:46 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/api/v1/hello-world-<int:variant>')
+def hello_world(variant):
+    return 'Hello World ' + str(variant)
+
+
+@app.route('/')
+def main_page():
+    return 'My Project'
+
+
+if __name__ == '__main__':
+    app.run()
