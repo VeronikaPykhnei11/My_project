c53110e10e971eece11bb7708bded83160029b87
Veronika <veronikaforkutsaa@gmail.com>
2020-12-09 14:47:28 +0200

    sqlalchemy

---
+++ myapp/test_db.py
@@ -0,0 +1,15 @@
+from myapp.models import *
+
+
+# f3 = Film(title='fffff', duration=1.24, rating=3.8)
+# f4 = Film(title='rrrr', duration=1.6, rating=1.5)
+#
+# admin1 = Admin(nickname='lor', firstname='a', lastname='b', email='aca', phone='1234', password='1111')
+#
+# hall1 = Hall(opacity=140)
+#
+# db.session.add(f3)
+# db.session.add(f4)
+# db.session.add(admin1)
+# db.session.add(hall1)
+# db.session.commit()
